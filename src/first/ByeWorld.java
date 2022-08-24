f537f73c5527dfdf5b41123fafbe107b3fa4bfcb
Dzesov Soslan <dzes91@gmail.com>
2022-08-24 13:12:50 -0400

    added ByeWorld.java

---
+++ src/first/ByeWorld.java
@@ -0,0 +1,8 @@
+package first;
+
+public class ByeWorld {
+    public static void main(String[] args) {
+        System.out.printf("Maybe 5 weeks of Java left");
+        System.out.printf("1.5 weeks of Git");
+    }
+}
